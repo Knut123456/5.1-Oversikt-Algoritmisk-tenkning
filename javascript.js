fd080db61ccaac8ca480beda01462728e7500086
Knut123456 <knroa002@osloskolen.no>
2024-03-04 10:43:03 +0100

    mye

--- javascript.js
+++ javascript.js
@@ -1,3 +1,35 @@
-function bilderdyr () {
-    
+let mat = 0
+let penger = 0
+
+function gimat() {
+    mat = mat + 1
+    penger = penger - 20
+    matmengde()
 }
+
+function inf() {
+    document.getElementById("trengermat").innerHTML = "dyren trenger mat" + mat
+}
+
+function matmengde() {
+    document.getElementById("trengermat").innerHTML = "dyren trenger mat" + mat
+}
+
+
+function bilderdyr() {
+    window.location.href = "dyr.html";
+}
+
+function oppgradering() {
+    window.location.href = "oppdatering.html";
+}
+
+function matlink() {
+    window.location.href = "mat.html";
+}
+
+function skatt() {
+        window.location.href = "skatt.html";
+    }
+
+
